eaaaab9d95d949a4957506c5726b54a51ac3c7aa
juanxo13597 <juanxo97master@gmail.com>
2024-03-16 17:29:43 +0100

    delete console log

--- src/pages/pokemon/pokemon-detail.tsx
+++ src/pages/pokemon/pokemon-detail.tsx
@@ -6,7 +6,6 @@ export function PokemonDetailPage() {
 	const params = useParams();
 	const url = HTTP_ENDPOINTS.Pokemon + "/" + params.id + "/";
 	const { data } = usePokemonDetail(url);
-	console.log(data);
 	return (
 		<div>
 			<img src={data?.sprites.front_default} alt={`pokemon ${data?.name}`} />
