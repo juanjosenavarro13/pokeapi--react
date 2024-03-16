6b400d22749718b31abb127d77baedb94ac8ef9e
juanxo13597 <juanxo97master@gmail.com>
2024-03-16 15:17:37 +0100

    initial layout styles

--- src/layouts/defaut-layout.tsx
+++ src/layouts/defaut-layout.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import styles from "./default-loyout.module.css";
 
 export function DefaultLayout() {
 	return (
-		<main>
-			<Outlet />
+		<main className={styles.main}>
+			<img className={styles.logo} src="./favicon.webp" alt="logo pokeapi" />
+			<div className={styles.container}>
+				<Outlet />
+			</div>
 		</main>
 	);
 }
