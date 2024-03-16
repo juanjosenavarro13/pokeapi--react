b5227a1f85858b4969b09518448938578ffb90ec
juanxo13597 <juanxo97master@gmail.com>
2024-03-16 14:01:11 +0100

    feat: :fire: install axios and tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./main.css";
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<App />
+			<ReactQueryDevtools initialIsOpen={import.meta.env.DEV} />
+		</QueryClientProvider>
 	</React.StrictMode>,
 );
