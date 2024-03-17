59c652df3f4aeb987d1d6f31c4e18980fce9e88c
juanxo13597 <juanxo97master@gmail.com>
2024-03-17 12:13:51 +0100

    feat: add footer

--- src/layouts/defaut-layout.tsx
+++ src/layouts/defaut-layout.tsx
@@ -2,6 +2,7 @@ import { Link, Outlet } from "react-router-dom";
 import styles from "./default-loyout.module.css";
 
 export function DefaultLayout() {
+	const urlGithub = "https://github.com/juanjosenavarro13/pokeapi--react";
 	return (
 		<main className={styles.main}>
 			<Link to={"/"}>
@@ -15,6 +16,20 @@ export function DefaultLayout() {
 			<div className={styles.container}>
 				<Outlet />
 			</div>
+			<footer>
+				<div
+					style={{
+						display: "flex",
+						flexWrap: "wrap",
+						justifyContent: "center",
+					}}
+				>
+					Todos los derechos reservados ~ Juan José Navarro -{" "}
+					<a href={urlGithub} style={{ color: "white" }}>
+						Github
+					</a>
+				</div>
+			</footer>
 		</main>
 	);
 }
